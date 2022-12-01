7f5c010ad7ef608f8f6aeeac7f68414558cd1421
Monica Doan <mtvdoan@gmail.com>
2022-12-01 14:44:21 -0800

    firstjsxreact

---
+++ MERN/REACT/firstreactwithjsx/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello Dojo</h1>
+        <h2>Things I need to do:</h2>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
